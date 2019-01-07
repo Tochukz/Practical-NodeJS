2278b48acf546c2442fffc6be96d59389dd8dd9a
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-01-08 00:15:32 +0200

    using the Express.js module

--- chp2/hello-express/server.js
+++ chp2/hello-express/server.js
@@ -9,5 +9,5 @@ app.listen(3000, () => { console.log("Open at localhost:3000")});
 /**
  * Here we have used a local install of express as a module by doing:
  * $ npm install express@4.15.4 --save --exact
- * See "express-app" to get a feel of what an express app with scaffolding looks like. 
+ * See "express-app" directory to get a feel of what an express app with scaffolding looks like. 
  */
