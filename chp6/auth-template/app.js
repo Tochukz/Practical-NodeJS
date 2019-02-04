7ffc4f2e0c43c69178eb03dd10d7536482a1885b
Tochukwu Nwachukwu <tnwachukwu@realmdigital.co.za>
2019-02-04 19:52:05 +0200

    auth template

---
+++ chp6/auth-template/app.js
@@ -0,0 +1,40 @@
+/**Defining dependencies */
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const auth = require('./auth');
+
+
+/**Adding a decorator */
+app.use((req, res, next) => {
+   req.error = false;
+   return next();    
+});
+
+/**Defining middleware */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/login', (req, res, next) => {
+    res.render('login', {message: req.messsage});
+});
+app.post('/login', auth.auth, (req, res, next) => {
+   if(req.error){
+       res.render('login', {errorMessage: req.errorMessage});
+   }
+   res.redirect('/home');
+});
+app.get('/home', (req, res, next) => {
+    res.render('home');
+});
+
+http.createServer(app).listen(app.get('port'), () => {
+    console.log(`Server running on localhost:${app.get('port')}`);
+});
---
+++ chp6/auth-template/auth.js
@@ -0,0 +1,15 @@
+module.exports.auth = (req, res, next) => {
+    const USER = 'chucks';
+    const PASS = '12345';
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password){
+        req.error = true;
+        req.errorMessage = "Username and password is required"; 
+        return next();
+    }
+    if(username === USER && password === PASS) {
+        return next();
+    }
+    res.status(401).send();
+};
